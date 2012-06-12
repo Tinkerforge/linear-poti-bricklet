43b6d6b4f44e0c9ca5e713cd8d67742848cbc675
Matthias Bolte <matthias@tinkerforge.com>
2012-06-12 18:57:04 +0200

    Prefer IPConnection destroy over join_thread

--- software/examples/csharp/ExampleCallback.cs
+++ software/examples/csharp/ExampleCallback.cs
@@ -27,7 +27,8 @@ class Example
 		// Register position callback to function PositionCB
 		lp.RegisterCallback(new BrickletLinearPoti.Position(PositionCB));
 
-		System.Console.WriteLine("Press ctrl+c to exit");
-		ipcon.JoinThread();
+		System.Console.WriteLine("Press key to exit");
+		System.Console.ReadKey();
+		ipcon.Destroy();
 	}
 }
