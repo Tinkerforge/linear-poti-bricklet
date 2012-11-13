9126a9712b580b10b6983ff23eac6a9a3010681b
Olaf Lüke <olaf@tinkerforge.com>
2012-11-13 10:03:43 +0100

    Support for protocol 2.0

--- software/src/config.h
+++ software/src/config.h
@@ -1,5 +1,5 @@
 /* linear-poti-bricklet
- * Copyright (C) 2010-2011 Olaf Lüke <olaf@tinkerforge.com>
+ * Copyright (C) 2010-2012 Olaf Lüke <olaf@tinkerforge.com>
  *
  * config.h: Linear Poti Bricklet specific configuration
  *
@@ -28,10 +28,15 @@
 #include "bricklib/drivers/board/sam3s/SAM3S.h"
 #include "poti.h"
 
-#define BRICKLET_HARDWARE_NAME "Linear Poti Bricklet 1.0"
-#define BRICKLET_FIRMWARE_VERSION_MAJOR 1
-#define BRICKLET_FIRMWARE_VERSION_MINOR 1
+#define BRICKLET_FIRMWARE_VERSION_MAJOR 2
+#define BRICKLET_FIRMWARE_VERSION_MINOR 0
 #define BRICKLET_FIRMWARE_VERSION_REVISION 0
+
+#define BRICKLET_HARDWARE_VERSION_MAJOR 1
+#define BRICKLET_HARDWARE_VERSION_MINOR 1
+#define BRICKLET_HARDWARE_VERSION_REVISION 0
+
+#define BRICKLET_DEVICE_IDENTIFIER 213
 
 #define LOGGING_LEVEL LOGGING_DEBUG
 #define DEBUG_BRICKLET 0
