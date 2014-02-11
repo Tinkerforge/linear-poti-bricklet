cf0c5067b15b1dfe29bc0b1d7b61abaf34ec56cc
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 12:13:56 +0100

    javascript: adding examples

--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -3,29 +3,29 @@ var BrickletLinearPoti = require('Tinkerforge/BrickletLinearPoti');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '5WL'; //Change to your UID
+var UID = '5WL';// Change to your UID
 
-var ipcon = new IPConnection(); //Create IP connection
-var poti = new BrickletLinearPoti(UID, ipcon); //Create device object
+var ipcon = new IPConnection(); // Create IP connection
+var poti = new BrickletLinearPoti(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ALREADY_CONNECTED) {
+        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
             console.log('Error: Already connected');        
         }
     }
-);//Connect to brickd
-//Don't use device before ipcon is connected
+);// Connect to brickd
+// Don't use device before ipcon is connected
 
-//Get current position of poti (return value has range 0-100)
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(IPConnection.CALLBACK_CONNECTED,    
     function(connectReason) {
+        // Get current position of poti (return value has range 0-100)
         poti.getPosition(
             function(position) {
                 console.log('Position: '+position);
             },
             function(error) {
-                if(error === IPConnection.RESPONSE_TIMED_OUT) {
+                if(error === IPConnection.ERROR_TIMEOUT) {
                   console.log('Error: The request timed out');
                 }
             }
@@ -37,8 +37,8 @@ console.log("Press any key to exit ...");
 process.stdin.on('data', function(data) {
 	    ipcon.disconnect(
             function(error) {
-                if(error === IPConnection.ALREADY_DISCONNECTED) {
-                    console.log('Error: Already disconnected');        
+                if(error === IPConnection.ERROR_NOT_CONNECTED) {
+                    console.log('Error: Not connected');        
                 }
             }
         );
