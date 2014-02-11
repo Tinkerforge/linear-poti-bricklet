0c575e715fb50fe25bf4e4ef70b1dc7f4af70cd2
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 15:41:25 +0100

    javascript: short examples

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -10,9 +10,7 @@ var poti = new BrickletLinearPoti(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
 );// Connect to brickd
 
@@ -35,14 +33,10 @@ poti.on(BrickletLinearPoti.CALLBACK_POSITION,
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 
--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -10,9 +10,7 @@ var poti = new BrickletLinearPoti(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
 );// Connect to brickd
 // Don't use device before ipcon is connected
@@ -25,23 +23,17 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
                 console.log('Position: '+position);
             },
             function(error) {
-                if(error === IPConnection.ERROR_TIMEOUT) {
-                  console.log('Error: The request timed out');
-                }
+                console.log('Error: '+error);
             }
         );
     }
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 
