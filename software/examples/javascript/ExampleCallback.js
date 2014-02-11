7f2b1bd9ebabc22c77f03e5315700c8ce7ebfc9d
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 12:20:34 +0100

    javascript: fixed error code and message

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -3,27 +3,32 @@ var BrickletLinearPoti = require('Tinkerforge/BrickletLinearPoti');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '5WL'; //Change to your UID
+var UID = '5WL';// Change to your UID
 
-var ipcon = new IPConnection(); //Create IP connection
-var poti = new BrickletLinearPoti(UID, ipcon); //Create device object
+var ipcon = new IPConnection();// Create IP connection
+var poti = new BrickletLinearPoti(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ALREADY_CONNECTED) {
+        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
             console.log('Error: Already connected');        
         }
     }
-);//Connect to brickd
-//Don't use device before ipcon is connected
+);// Connect to brickd
 
-//Set Period for position callback to 0.05s (50ms)
-//Note: The position position is only called every 50ms if the 
-//position has changed since the last call!
-poti.setPositionCallbackPeriod(50);
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Set Period for position callback to 0.05s (50ms)
+        // Note: The position position is only called every 50ms if the 
+        // position has changed since the last call!
+        poti.setPositionCallbackPeriod(50);   
+    }
+);
 
-//Register position callback
+// Register position callback
 poti.on(BrickletLinearPoti.CALLBACK_POSITION,
+    // Callback function for position callback (parameter has range 0-100)
     function(position) {
         console.log('Position: '+position);
     }
@@ -33,8 +38,8 @@ console.log("Press any key to exit ...");
 process.stdin.on('data', function(data) {
 	    ipcon.disconnect(
             function(error) {
-                if(error === IPConnection.ALREADY_DISCONNECTED) {
-                    console.log('Error: Already disconnected');        
+                if(error === IPConnection.ERROR_NOT_CONNECTED) {
+                    console.log('Error: Not connected');        
                 }
             }
         );
