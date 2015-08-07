c33d3b054bdd521f9fd13a42f493a2e4fbf42c8b
Matthias Bolte <matthias@tinkerforge.com>
2015-08-07 10:18:24 +0200

    Use full device class name in Python examples

--- software/examples/python/example_simple.py
+++ software/examples/python/example_simple.py
@@ -6,11 +6,11 @@ PORT = 4223
 UID = "XYZ" # Change to your UID
 
 from tinkerforge.ip_connection import IPConnection
-from tinkerforge.bricklet_linear_poti import LinearPoti
+from tinkerforge.bricklet_linear_poti import BrickletLinearPoti
 
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection
-    lp = LinearPoti(UID, ipcon) # Create device object
+    lp = BrickletLinearPoti(UID, ipcon) # Create device object
 
     ipcon.connect(HOST, PORT) # Connect to brickd
     # Don't use device before ipcon is connected
