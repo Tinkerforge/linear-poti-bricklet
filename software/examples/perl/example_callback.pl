16298e532943d734ef05b5dee8e1a01bf60f0b40
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 16:03:06 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -1,35 +1,34 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletLinearPoti;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '5WL'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $poti = Tinkerforge::BrickletLinearPoti->new(&UID, $ipcon); # Create device object
+my $lp = Tinkerforge::BrickletLinearPoti->new(&UID, $ipcon); # Create device object
 
-# Callback function for position callback (parameter has range 0-100)
+# Callback subroutine for position callback (parameter has range 0 to 100)
 sub cb_position
 {
     my ($position) = @_;
 
-    print "Position: $position\n";
+    print "Position: " . $position . "\n";
 }
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Set Period for position callback to 0.05s (50ms)
-# Note: The position position is only called every 50ms if the 
-#       position has changed since the last call!
-$poti->set_position_callback_period(50);
+# Set period for position callback to 0.05s (50ms)
+# Note: The position callback is only called every 0.05 seconds
+#       if the position has changed since the last call!
+$lp->set_position_callback_period(50);
 
-# Register position callback to function cb_position
-$poti->register_callback($poti->CALLBACK_POSITION, 'cb_position');
+# Register position callback to subroutine cb_position
+$lp->register_callback($lp->CALLBACK_POSITION, 'cb_position');
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -1,23 +1,22 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletLinearPoti;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '5WL'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $poti = Tinkerforge::BrickletLinearPoti->new(&UID, $ipcon); # Create device object
+my $lp = Tinkerforge::BrickletLinearPoti->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Get current position of poti (return value has range 0-100)
-my $position = $poti->get_position();
-print "Position: $position\n";
+# Get current position (range is 0 to 100)
+my $position = $lp->get_position();
+print "Position: " . $position . "\n";
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
